574c3421be877eac84272d3bd5d3cb72d889b372
Brad Fisher <bradfisher95dev@gmail.com>
2024-06-29 20:18:32 +0100

    setup eslint & prettier, need to resolve issues with app.spec.tsx linting

--- apps/ether-app/src/app/app.tsx
+++ apps/ether-app/src/app/app.tsx
@@ -1,10 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import React from 'react';
 
 export function App() {
   return (
     <div>
-      <p>Hello</p>
+      <p>Test</p>
     </div>
   );
 }
