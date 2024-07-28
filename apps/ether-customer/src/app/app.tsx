bac3ddbe26af7c0bc9d889e2561d12b1f6f19f87
Brad Fisher <bradfisher95dev@gmail.com>
2024-07-28 16:23:03 +0100

    implemented an approach for dynamic themes using this slice

--- apps/ether-customer/src/app/app.tsx
+++ apps/ether-customer/src/app/app.tsx
@@ -1,41 +1,11 @@
 import '@ether/styles';
-import { useFetchThemeQuery } from '@ether/dynamic-theme';
-import { useState } from 'react';
+import { DynamicTheme } from '@ether/dynamic-theme';
 
 export function App() {
-  const [theme, setTheme] = useState('dark');
-
-  const { data, isLoading, isSuccess, isError } = useFetchThemeQuery(theme);
-
-  let content;
-
-  if (isLoading) {
-    content = <div>Loading!</div>;
-  } else if (isSuccess) {
-    content = <div>Retrieved theme successfully.</div>;
-  } else if (isError) {
-    content = <div>Error!</div>;
-  }
-
   return (
     <div>
-      <p>Customer</p>
-      <br />
-      <div>
-        <p>Theme to fetch:</p>
-        <select
-          value={theme}
-          onChange={(e) => setTheme(String(e.target.value))}
-        >
-          <option value="dark">Dark</option>
-          <option value="light">Light</option>
-          <option value="crazy">Crazy</option>
-        </select>
-      </div>
-      <br />
-      {content}
-      <br />
-      <div>Theme: {data?.backgroundColor};</div>
+      <h1>Customer</h1>
+      <DynamicTheme></DynamicTheme>
     </div>
   );
 }
--- libs/shared/features/dynamic-theme/src/lib/dynamic-theme.tsx
+++ libs/shared/features/dynamic-theme/src/lib/dynamic-theme.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useFetchThemeQuery } from './slices/themeSlice';
+
 export function DynamicTheme() {
+  const [theme, setTheme] = useState('dark');
+  const { data } = useFetchThemeQuery(theme);
+
+  useEffect(() => {
+    if (data) {
+      const root = document.querySelector(':root') as any;
+      root.style.setProperty('--background-color', data.backgroundColor);
+    }
+  });
+
+  const updateTheme = () => {
+    theme === 'dark' ? setTheme('light') : setTheme('dark');
+  };
+
   return (
     <div>
-      <h1>Welcome to DynamicTheme!</h1>
+      <h1>Theme: {data?.backgroundColor}</h1>
+      <button onClick={updateTheme}>Swap Theme</button>
     </div>
   );
 }
