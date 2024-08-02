cfdfceeb1b50121f3ecf8863e2ef9a62d59720d9
Brad Fisher <bradfisher95dev@gmail.com>
2024-08-02 19:43:22 +0100

    attempted to add fonts but was unsuccessful - will investigate later

--- apps/ether-customer/vite.config.ts
+++ apps/ether-customer/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname,
@@ -10,6 +11,9 @@ export default defineConfig({
   server: {
     port: 4201,
     host: 'localhost',
+    fs: {
+      strict: false,
+    },
   },
 
   preview: {
